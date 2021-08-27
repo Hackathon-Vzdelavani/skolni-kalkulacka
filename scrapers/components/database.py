0c300f1dc88f3df5ec387dd6764886c0f4fac6d5
Externista <externista@ComtesFHT.local>
2021-08-27 23:11:24 +0200

    Program to db insertion.

--- scrapers/components/database.py
+++ scrapers/components/database.py
@@ -1,12 +1,14 @@
 from peewee import *
 import os
 import sys
+from typing import Dict, List, Any
 
 
 class Program(Model):
     name = CharField()
-    faculty = ForeignKeyField(Faculty, backref="program")
-    catalogue_url = CharField()
+    faculty = CharField()
+    # faculty = ForeignKeyField(Faculty, backref="program")
+    catalog_url = CharField()
     #program_type = CharField()
     #length = IntegerField()
     #language = CharField()
@@ -29,22 +31,31 @@ def init_db(db: SqliteDatabase) -> None:
     """
     Bind the models to the database.
     """
-    db.bind([University, Faculty, Program, Course])   
-    db.create_tables([University, Faculty, Program, Course])
+    db.bind([Program])   
+    db.create_tables([Program])
     db.close()
+
+
+def insert_program(db: SqliteDatabase, data: Dict[str, Any]) -> None:
+    program, _ = Program.get_or_create(
+        name = data["name"],
+        faculty = data["faculty"],
+        catalog_url = data["catalog_url"],
+    )
 
 
 def insert_skill(db: SqliteDatabase) -> None:
     ...
+
 
 def insert_course(db: SqliteDatabase) -> None:
     db.connect()
     
     return True
 
-
-
 if __name__ == "__main__":
     parent_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     database_path = os.path.join(parent_directory, "data", "kalkulacka.db")
     db = SqliteDatabase(database_path, pragmas={'foreign_keys': 1})
+    init_db(db)
+    insert_program(db, {"name":"name", "faculty":"faculty", "catalog_url":"catalog_url"})
