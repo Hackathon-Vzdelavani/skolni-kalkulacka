a5e8920d1388d374da2c896ac8fad41397b7392e
basta <jsembasta@gmail.com>
2021-08-27 23:10:53 +0200

    fixed merge

--- scrapers/spiders/program_spider.py
+++ scrapers/spiders/program_spider.py
@@ -31,25 +31,38 @@ class ProgramSpider(scrapy.Spider):
             time.sleep(1)
             program_wrapper = self.driver.find_element_by_css_selector('div.article-item')
             selector = Selector(text=program_wrapper.get_attribute('innerHTML'))
-            self.extract_program_urls(selector)
+            self.extract_program(selector)
         except Exception as e:
             self.logging.error("Error getting body" + error_message(e))
 
     def test_url(self, url):
         return re.match("/cs/Admission/Study-fields/index.html", url)
 
-    def extract_program_urls(self, selector):
+    def extract_program(self, selector):
         programs = selector.css("li.typo-base-small")
         for program in programs:
-            link = program.css("a.head-bold-active::attr(href)").extract()
-            name = program.css("a.head-bold-active::text").extract()
-            faculty = program.css("div.faculty-tag.primary-fpe::text").extract()
+            link = program.css("a.head-bold-active::attr(href)").extract_first()
+            name = program.css("a.head-bold-active::text").extract_first()
+            faculty = program.css("div.faculty-tag.primary-fpe::text").extract_first()
             tags = program.css("div.faculty-tag::text").extract()
-            url = "https://www.zcu.cz/" + link
+            tags = ",".join(tags)
+            url = "https://www.zcu.cz" + link
             if self.test_url(url) and url not in self.program_urls:
                 self.logging.info(name)
                 item = {"url": url, "name": name, "faculty": faculty, "tags": tags}
-                print(item)
+                self.extract_program_url(item)
                 self.program_urls.append(url)
                 #self.database.insert_program(item)
 
+    def extract_program_url(self, item):
+        self.logging.info('Visiting program URL ' + item["url"])
+        self.driver.get(item["url"])
+        try:
+            time.sleep(1)
+            body = self.driver.find_element_by_css_selector('body')
+            selector = Selector(text=body.get_attribute('innerHTML'))
+            detail_link = selector.css("a#detailButton:attr(href)").extract()
+            return detail_link
+        except Exception as e:
+            self.logging.error("Error getting body" + error_message(e))
+            return ""
